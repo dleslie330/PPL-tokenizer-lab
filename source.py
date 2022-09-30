f28a09ea520150d7130085096f6374436e72489c
Dakota Leslie <dleslie@cub.uca.edu>
2022-09-29 22:36:00 -0500

    It's almost there. Some words get dropped off, and the string constants are messing with the token value for some reason. Also have to fix skipping comments in the SquareGame file

--- source.py
+++ source.py
@@ -6,29 +6,69 @@ from xml.dom import minidom
 
 def write(elem):
     xmlstr = minidom.parseString(ET.tostring(elem.getroot())).toprettyxml(indent=" ")
-    with open("SquareGameOutput.xml", "w") as f:
+    with open("MainOutput.xml", "w") as f:
         f.write(xmlstr)
 
+def add(et, str, val):
+    temp = ET.Element(str)
+    temp.text = " " + val + " "
+    et.getroot().append(temp)
+    return None
+
 def driver():
-    read = "SquareGame.jack"
+    read = "Main.jack"
     output = ET.ElementTree(ET.Element("tokens"))
+    keywords = {"class", "constructor", "method", "function", "int", "boolean", "char", "void", "var", "static", "field", "let", "do", "if", "else", "while", "return", "true", "flase", "null", "this"}
+    symbols = {'(', ')', '[', ']', '{', '}', ',', ';', '=', '.', '+', '-', '*', '/', '&', '|', '~', '<', '>'}
+    flag = False
 
-    with open("SquareGame.jack") as f:
-        while (True):
-            line = f.readline()
+    with open(read) as f:
+            lines = f.readlines()
+            for line in lines:
+                if (flag):
+                    if (line[-2] == '*' and line[-1] == '/'):
+                        flag = False
+                    continue
 
-            if not line:
-                break
-
-            token = ''
-            for i in range(len(line)):
-                if (line[i] == '(' or line[i] == ')' or line[i] == '[' or line[i] == ']' or line[i] == '{' or line[i] =='}' or line[i] == ',' or line[i] == ';' or line[i] == '=' or line[i] == '.' or line[i] == '+' or line[i] == '-' or line[i] == '*' or line[i] == '/' or line[i] == '&' or line[i] == '|' or line[i] == '~' or line[i] == '<' or line[i] == '>'):
-                    token = line[i]
-                    temp = ET.Element("symbol")
-                    temp.text = token
-                    output.getroot().append(temp)
-                elif (line[i] == ' '):
-                    token = ''
+                token = ''
+                for i in range(len(line)):
+                    if (line[i] == '/' and line[i + 1] == '/'):
+                        break
+                    elif(line[i] == '/' and line[i + 1] == '*'):
+                        #flag = True
+                        break
+                    elif(line[i] == '"'):
+                        token = ''
+                        j = i + 1
+                        while (j < len(line) and line[j] != '"'):
+                            token = token + line[j]
+                            j = j + 1
+                        add(output, "stringConstant", token)
+                        token = ''
+                        if (j + 1 >= len(line)):
+                            break
+                        else:
+                            i = j + 1
+                    elif (line[i] in symbols):
+                        token = line[i]
+                        add(output, "symbol", token)
+                        token = ''
+                    elif (line[i] == ' ' or (i + 1) >= len(line) or (line[i + 1] in symbols)):
+                        if (i + 1 >= len(line) or (line[i + 1] in symbols)):
+                            token = token + line[i]
+                        if (token == ''):
+                            pass
+                        elif (token in keywords):
+                            add(output, "keyword", token)
+                        elif (token.isnumeric()):
+                            add(output, "integerConstant", token)
+                        elif ((token[0] == '_' or token[0].isalpha()) and token.isalnum()):
+                            add(output, "identifier", token)
+                        token = ''
+                    if (i < len(line) and line[i] != ' '):
+                        token = token + line[i]
+                    else:
+                        token = ''
     write(output)
 
 driver()
